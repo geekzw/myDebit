2419ca5d389806c782abf5aea6cc960e621ee68b
古剑 <gujian@maihaoche.com>
2018-07-10 21:50:37 +0800

    解决跨域

---
+++ debit-web/src/main/java/com/gzw/debit/web/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gzw.debit.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * auth:gujian
+ * time:2018/7/10
+ * email:gujian@maihaoche.com
+ * describe:
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
