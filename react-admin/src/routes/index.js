8b952de486510a51dfa1f0bf871aa39ed6b80709
tanjunhao <244296129@qq.com>
2018-07-11 22:41:46 +0800

    修改登录权限。

--- react-admin/src/routes/index.js
+++ react-admin/src/routes/index.js
@@ -13,9 +13,10 @@ export default class CRouter extends Component {
     componentWillMount() {
         var user = JSON.parse(localStorage.getItem("user"));
         if (user && user.type == 0){
-            routesConfig["menus"].push({
-                key: '/app/table', title: '商家列表', icon: 'copy',component: 'MerchantList',
-            });
+            routesConfig["menus"].concat([
+                {   key: '/app/merchantList', title: '商家列表', icon: 'copy',component: 'MerchantList'    },
+                {   key: '/app/table', title: '编辑商家', icon: 'copy',component: 'MerchantList'    },
+            ]);
         }
         this.setState({
             configs: routesConfig
@@ -25,13 +26,17 @@ export default class CRouter extends Component {
         const { auth } = this.props;
         const { permissions } = auth.data;
         // const { auth } = store.getState().httpData;
-        if (!permissions || !permissions.includes(permission.sessionId)) return <Redirect to={'404'} />;
+        if (!permissions || !permissions.sessionId) return <Redirect to={'404'} />;
         return component;
     };
     requireLogin = (component, permission) => {
         const { auth } = this.props;
         const { permissions } = auth.data;
-        if (process.env.NODE_ENV === 'production' && !permissions.sessionId) { // 线上环境判断是否登录
+        console.log(permissions);
+        // if (process.env.NODE_ENV === 'production' && !permissions && !permissions.sessionId) { // 线上环境判断是否登录
+        //     return <Redirect to={'/login'} />;
+        // }
+        if (!permissions || !permissions.sessionId) { // 线上环境判断是否登录
             return <Redirect to={'/login'} />;
         }
         return permission ? this.requireAuth(permission, component) : component;
