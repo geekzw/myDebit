ee1b043ebb39a4d3dca55f8b102128f90fea3144
tanjunhao <244296129@qq.com>
2018-07-30 21:26:03 +0800

    修复侧边栏缩小状态bug，修改资源文件配置，

--- debit-web/src/main/java/com/gzw/debit/web/config/MyWebAppConfigurer.java
+++ debit-web/src/main/java/com/gzw/debit/web/config/MyWebAppConfigurer.java
@@ -4,6 +4,7 @@ import com.gzw.debit.web.interceptor.LoginInterceptor;
 import com.gzw.debit.web.interceptor.SmsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -27,5 +28,18 @@ public class MyWebAppConfigurer implements WebMvcConfigurer {
         registry.addInterceptor(new SmsInterceptor())
                 .addPathPatterns("/getSmsCode.json");
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(!registry.hasMappingForPattern("/**")){
+            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        }
+        if(!registry.hasMappingForPattern("/app/**")){
+            registry.addResourceHandler("/app/**").addResourceLocations("classpath:/static/app/");
+        }
+        if(!registry.hasMappingForPattern("/admin/**")){
+            registry.addResourceHandler("/admin/**").addResourceLocations("classpath:/static/admin/");
+        }
     }
 }
